3c33d88bd879ee9f28aaca07c41bbc68bca397d6
Alan Cintra <alancintra7@gmail.com>
2020-10-16 08:53:05 -0300

    Products

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,30 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    // await this.productsRepository.updateQuantity([
+    //   { id: '17bfd563-c12e-4719-a79e-f3685f4c47de', quantity: 300 },
+    //   { id: '4cf7ef1f-b5df-48b1-8aa8-4eb63b4d0bf6', quantity: 300 },
+    // ]);
+
+    const checkProduct = await this.productsRepository.findByName(name);
+
+    if (checkProduct) {
+      throw new AppError('Product already exists');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
